e56ffcfa72c9624a27785a3871ac6d483f318587
Ancwwx <ancwwx@gmail.com>
2023-05-15 20:41:34 +0900

    no message

--- src/main/java/codecademyPractice/stacks/PizzaDelivery.java
+++ src/main/java/codecademyPractice/stacks/PizzaDelivery.java
@@ -6,7 +6,7 @@ public class PizzaDelivery {
     public Stack pizzaHouse;
 
     public PizzaDelivery() {
-        // 1. Instantiate deliveryGal and pizzaHouse stacks below
+
         this.deliveryGal = new Stack(5);
         this.pizzaHouse = new Stack();
 
@@ -15,12 +15,12 @@ public class PizzaDelivery {
     public void assign(String [] pizzas) {
         for (String pizza : pizzas) {
             try {
-                // 2. Push pizzas onto deliveryGal and print the update
+                
                 this.deliveryGal.push(pizza);
                 System.out.println(pizza + " pizza added to deliveryGal stack.");
                 
             } catch (Error e) {
-                // 3. Push pizzas onto pizzaHouse and print the update
+  
                 this.pizzaHouse.push(pizza);
                 System.out.println("deliveryGal left to make deliveries! " + pizza + " pizza added to pizzaHouse stack.");
             }
@@ -31,7 +31,7 @@ public class PizzaDelivery {
     public void deliver() {
         int numPizzas = this.deliveryGal.size;
         for (int i = 0; i < numPizzas; i++) {
-            // 4. Pop off each pizza from deliveryGal and print the update
+      
             String pizzaType = this.deliveryGal.pop();
             System.out.println(pizzaType + " pizza delivered!");
         }
