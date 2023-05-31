41fc0686c7fd2c65d83bb7522f9db254ecae3d71
Ancwwx <ancwwx@gmail.com>
2023-05-31 21:58:31 +0900

    no message

--- src/main/java/codecademyPractice/test.java
+++ src/main/java/codecademyPractice/test.java
@@ -2,23 +2,20 @@ package codecademyPractice;
 
 public class test {
 	public static void main(String[] args) {
-//		   double[][] data = {{1.4, 7.5, 3.5, 8.7, 9.2},{0.8, 2.6, 6.3, 3.4, 7.5}};
-//	        
-//	        for(int i = 0; i < data[1].length; i++) {
-//	            for( int j = 0; j < data.length; j++) {
-//	                System.out.println(data[j][i]);
-//	            }
-//	        }
-//	        
-//	        
-	        int index=1;
-	        //for(int i=0;i<10;i++) {
-	        	index=index++;
-	       // }
-	        System.out.println(index);
-	        System.out.println(index);
-	        System.out.println(index);
-	        
+		double[][] data = { { 1.4, 7.5, 3.5, 8.7, 9.2 }, { 0.8, 2.6, 6.3, 3.4, 7.5 } };
+
+		for (int i = 0; i < data[1].length; i++) {
+			for (int j = 0; j < data.length; j++) {
+				System.out.println(data[j][i]);
+			}
+		}
+
+		int index = 1;
+		for (int i = 0; i < 10; i++) {
+			index = index++;
+		}
+		System.out.println(index);
+
 	}
 
 }
